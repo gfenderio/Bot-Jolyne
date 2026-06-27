fcc2dfeb0a5fd28e3176e500b963ed4dffb23ac5
Gilang Fenderio <30297260+gfenderio@users.noreply.github.com>
2026-06-27 18:54:21 +0700

    fix: remove dead deliveree imports so tsc build passes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { env } from "./config/env.js";
-import { DiscordWebhookNotifier } from "./deliveree/discordNotifier.js";
-import {
-  activeMockDelivereeBookingIds,
-  mockDelivereeClient,
-  mockDelivereeStateStore
-} from "./deliveree/mockRuntime.js";
-import { startDelivereePoller } from "./deliveree/poller.js";
-import { startDelivereeExtensionIntake } from "./deliveree/extensionIntake.js";
-import { startDelivereeWebMonitor } from "./deliveree/webMonitor.js";
 import { handleInteractionCreate } from "./events/interaction-create.js";
 import { handleReady } from "./events/ready.js";
 import { startBirthdayNowScheduler } from "./schedulers/birthday-now.js";
@@ -16,16 +7,8 @@ import { startMachitanDailyReportScheduler } from "./machitan/dailyReportSchedul
 import { registerGuildSlashCommands } from "./services/slash-commands.js";
 import { startNotionStandupScheduler } from "./schedulers/notion-standup.js";
 
-startDelivereePoller({
-  activeBookingIds: activeMockDelivereeBookingIds,
-  client: mockDelivereeClient,
-  intervalMs: env.POLL_INTERVAL_SECONDS * 1000,
-  notifier: new DiscordWebhookNotifier(env.DISCORD_WEBHOOK_URL),
-  stateStore: mockDelivereeStateStore
-});
-
 if (!env.DISCORD_TOKEN) {
-  console.warn("DISCORD_TOKEN belum diisi. Discord bot client dilewati; Deliveree mock poller tetap berjalan.");
+  console.warn("DISCORD_TOKEN belum diisi. Discord bot client dilewati.");
 } else {
   const client = new Client({
     intents: [GatewayIntentBits.Guilds]
@@ -34,8 +17,6 @@ if (!env.DISCORD_TOKEN) {
   client.once(Events.ClientReady, (readyClient) => {
     handleReady(readyClient);
     startBirthdayNowScheduler(readyClient);
-    startDelivereeExtensionIntake(readyClient);
-    startDelivereeWebMonitor(readyClient);
     startMachitanDailyReportScheduler(readyClient);
     startNotionStandupScheduler(readyClient);
   });
