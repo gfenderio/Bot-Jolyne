ea6da864bd493e9f1067919abd9b48e435dd972f
Gilang Fenderio <30297260+gfenderio@users.noreply.github.com>
2026-06-27 18:43:21 +0700

    remove deliveree commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,6 @@
 import type { SlashCommand } from "../types/command.js";
 import { command as birthday } from "./birthday.js";
 import { command as birthdayNow } from "./birthday-now.js";
-import { command as delivereeCase } from "./deliveree-case.js";
-import { command as delivereeCases } from "./deliveree-cases.js";
-import { command as delivereeStatus } from "./deliveree-status.js";
-import { command as delivereeExtensionHealth } from "./deliveree-extension-health.js";
 import { command as ping } from "./ping.js";
 import { command as server } from "./server.js";
 import { command as whoami } from "./whoami.js";
@@ -18,10 +14,6 @@ for (const command of [
   birthday,
   birthdayNow,
   whoami,
-  delivereeCase,
-  delivereeCases,
-  delivereeStatus,
-  delivereeExtensionHealth,
   task
 ]) {
   commands.set(command.data.name, command);
